6e6c7515379923fe00155e600edae1bae33ad17f
Arkinefed <arkinefedmail@gmail.com>
2022-06-04 14:28:57 +0200

    added end screens and set window title

--- src/main/java/fiveMens/Main.java
+++ src/main/java/fiveMens/Main.java
@@ -6,31 +6,26 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends  Application {
+public class Main extends Application {
 
-	public static void main( String[] args) {
+	public static void main(String[] args) {
 		launch(args);
-		
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		FXMLLoader loader = new FXMLLoader();
-		
+
 		loader.setLocation(getClass().getResource("/fxml/test.fxml"));
-		
+
 		VBox vBox = loader.load();
-		
+
 		Scene scene = new Scene(vBox);
-		
-		
-		
+
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("pierwsze okno");
+		primaryStage.setTitle("Five Men's Morris");
+		primaryStage.setResizable(false);
 		primaryStage.show();
-		
 	}
-		
-	
 }
