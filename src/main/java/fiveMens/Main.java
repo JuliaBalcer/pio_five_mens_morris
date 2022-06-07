7e2c33dead190885da2dfd2b437c860858de6ba1
Arkinefed <arkinefedmail@gmail.com>
2022-06-07 22:52:31 +0200

    added game icon and changed window title

--- src/main/java/fiveMens/Main.java
+++ src/main/java/fiveMens/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,8 +20,8 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
-        stage.setTitle("FiveMensMorris");
-
+        stage.setTitle("Five Men's Morris");
+        stage.getIcons().add(new Image(getClass().getResource("/fxml/black_tile_selected.png").toExternalForm()));
         stage.setResizable(false);
     }
 
